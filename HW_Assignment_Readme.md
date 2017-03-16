120d4f4030866869eecbab2676517246e312e2d8
Arleta Bilous <arletka@Arletas-MacBook-Air-2.local>
2017-03-16 16:48:04 -0700

    Added logic

---
+++ HW_Assignment_Readme.md
@@ -0,0 +1,38 @@
+# HW - Hangman
+
+I have build advance version of Trivia Game  jquery. 
+
+
+## Live Link
+ - https://github.com/abioous/GifTastic/blob/master/index.html
+
+
+## Description on how to use the app
+
+Go to the live link to see page design.
+
+## Requirements
+####     Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
+
+    We chose animals for our theme, but you can make a list to your own liking.
+
+    Your app should take the topics in this array and create buttons in your HTML.
+
+    Try using a loop that appends a button for each string in the array.
+
+    When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+
+    When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+    Under every gif, display its rating (PG, G, so on).
+
+    This data is provided by the GIPHY API.
+    Only once you get images displaying with button presses should you move on to the next step.
+
+    Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
+
+
+## Technologies Used
+
+- HTML5 for web pages,CSS
+- Java Script and JQuery for game logic and rendering.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,98 @@
 var topics = ["cat", "dog", "parrot", "rabbit"];
 
 var app = {
-	topics:[],
-	render:function(){
-		var buttons = $('#buttons');
-		buttons.empty();
-		for(var i =0;i<this.topics.length;i++){
-			var button = $('<button>');
-			button.text(this.topics[i]);
-			buttons.append(button);
+    topics:[],
+    render:function(){
+        var buttons = $('#buttons');
+        buttons.empty();
+        for(var i =0;i<this.topics.length;i++){
+            var button = $('<button>');
+            button.text(this.topics[i]);
+            button.attr('data-topic', this.topics[i])
+            buttons.append(button);
+            button.click(function() {
+                var topic = $(this).attr('data-topic');
+                app.getTopicInfo(topic)
+            })
 
-		}
-	},
+        }
+    },
+    renderTopic:function(data) {
+        console.log(data);
+        $('#images').empty();
+        for(var i = 0;i<data.length;i++) {
+             var item = $("<div>");
+            // Creating a paragraph tag with the result item's rating
+            var rating = $("<p>").text("Rating: " + data[i].rating);
 
-	addTopic:function(topic){
-		this.topics.push(topic);
-		this.render();
+            // Creating and storing an image tag
+            var image = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            image.attr("src", data[i].images.fixed_height_still.url);
+            image.attr("data-state", "still"); //assign original image type
 
-	},
+            //set attribute for  still image url
+            image.attr("data-still-image-url", data[i].images.fixed_height_still.url);
+            
+            //set attribute for animated image url
+            image.attr("data-animated-image-url", data[i].images.fixed_height.url);
+            
+            //append rating
+            item.append(rating);
+            //append image
+            item.append(image);
+            //add flex at 33% with wrap
+            item.addClass('flex-33');
+            //swap image function
+            image.click(function() {
+                //if still set image src to animated image and update state
+                if($(this).attr('data-state') == 'still') {
+                    $(this).attr("src", $(this).attr("data-animated-image-url"));
+                    $(this).attr('data-state', 'animated')
+                } else {
+                    //set image src with still image url and update state
+                    $(this).attr("src", $(this).attr("data-still-image-url"));
+                    $(this).attr('data-state', 'still')
+                }
+            })
 
-	start:function(topics){
-		this.topics = topics;
-		this.render();
-	},
+            $('#images').append(item)
+        }    
+    },
+    getTopicInfo:function(topic) {
+         // Constructing a queryURL using the topic
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      // Performing an AJAX request with the queryURL
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })// After data comes back from the request
+        .done(function(response) {
+            app.renderTopic(response.data)
+        })
+    },
+    addTopic:function(topic){
+        this.topics.push(topic);
+        this.render();
+
+    },
+
+    start:function(topics){
+        this.topics = topics;
+        this.render();
+    },
 
 };
 
+
 $(document).ready(function(){
-	var submit = $('#submit');
-	submit.click(function(){
-		var topic = $('#name').val();
-		app.addTopic(topic);
-		
-	})
-	app.start(topics);
-
-
+    var submit = $('#submit');
+    submit.click(function(){
+        var topic = $('#name').val();
+        app.addTopic(topic);
+        
+    })
+    app.start(topics);
 })
-
